720f5bfd173d9cb22ccbf12fb1bba332096db2b9
SMcGrath714277 <714277@student.dusd.net>
2023-09-07 13:48:23 -0700

    Update
    Completed up to Lesson 4.3 - 4.

--- SMP6Unit4GameplayMechanics/Assets/Scripts/PlayerController.cs
+++ SMP6Unit4GameplayMechanics/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private GameObject focalPoint;
     private Rigidbody playerRb;
+
+    private float powerupStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPowerup;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,24 @@ public class PlayerController : MonoBehaviour
     {
         float fowardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * fowardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+            hasPowerup = true;
+            Destroy(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
